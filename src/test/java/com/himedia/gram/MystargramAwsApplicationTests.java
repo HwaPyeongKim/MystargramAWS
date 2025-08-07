eeba127484ca9637af56ba8f18bb448198e21d46
HwaPyeongKim <47729865+HwaPyeongKim@users.noreply.github.com>
2025-08-07 15:51:43 +0900

    first commit

---
+++ src/test/java/com/himedia/gram/MystargramAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.himedia.gram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MystargramAwsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
